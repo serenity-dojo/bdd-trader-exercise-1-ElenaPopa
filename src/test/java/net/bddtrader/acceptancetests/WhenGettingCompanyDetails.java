7b583f23d68dc4911b2ef66826ce4d80708de64b
Elena <elenageorgeta.popa@gmail.com>
2024-10-13 12:51:50 +0100

    refactor baseUri into it's own method and set the public and localhost urls

--- src/test/java/net/bddtrader/acceptancetests/WhenGettingCompanyDetails.java
+++ src/test/java/net/bddtrader/acceptancetests/WhenGettingCompanyDetails.java
@@ -1,16 +1,27 @@
 package net.bddtrader.acceptancetests;
 
 import io.restassured.RestAssured;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.equalTo;
 
 public class WhenGettingCompanyDetails {
+
+    @Before
+    public void set_rest_baseuri(){
+        //localhost
+        RestAssured.baseURI = "http://localhost:9000/api";
+
+        //public API
+        //RestAssured.baseURI = "https://bddtrader.herokuapp.com/api";
+    }
     @Test
     public void should_return_name_and_sector() {
-        RestAssured.get("http://localhost:9000/api/stock/aapl/company")
-                .then()
-                .body("companyName", equalTo("Apple, Inc."))
-                .body("sector", equalTo("Electronic Technology"));
+
+        RestAssured.get("/stock/aapl/company")
+            .then()
+            .body("companyName", equalTo("Apple, Inc."))
+            .body("sector", equalTo("Electronic Technology"));
     }
 }
