cd91bbb80dd24454cd2d874dba52ff4c75449826
Elena <elenageorgeta.popa@gmail.com>
2024-10-13 14:13:39 +0100

    create example test method that uses pathParam

--- src/test/java/net/bddtrader/acceptancetests/WhenGettingCompanyDetails.java
+++ src/test/java/net/bddtrader/acceptancetests/WhenGettingCompanyDetails.java
@@ -4,6 +4,7 @@ import io.restassured.RestAssured;
 import org.junit.Before;
 import org.junit.Test;
 
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
 
 public class WhenGettingCompanyDetails {
@@ -11,7 +12,7 @@ public class WhenGettingCompanyDetails {
     @Before
     public void set_rest_baseuri(){
         //localhost
-        RestAssured.baseURI = "http://localhost:9000/api";
+        baseURI = "http://localhost:9000/api";
 
         //public API
         //RestAssured.baseURI = "https://bddtrader.herokuapp.com/api";
@@ -19,9 +20,19 @@ public class WhenGettingCompanyDetails {
     @Test
     public void should_return_name_and_sector() {
 
-        RestAssured.get("/stock/aapl/company")
+        get("/stock/aapl/company")
             .then()
             .body("companyName", equalTo("Apple, Inc."))
             .body("sector", equalTo("Electronic Technology"));
+    }
+
+    @Test
+    public void should_return_name_and_sector_using_path_param() {
+        given().pathParam("symbol", "aapl")
+                .when()
+                .get("/stock/{symbol}/company")
+                .then()
+                .body("companyName", equalTo("Apple, Inc."))
+                .body("sector", equalTo("Electronic Technology"));
     }
 }
